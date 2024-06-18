965aecbcbce818e509f97afb682135f3bf607bab
lorenzo <lorenzo@gmail.com>
2024-06-18 17:02:56 -0300

    commit

--- recuperar.php
+++ recuperar.php
@@ -21,3 +21,23 @@ $token = bin2hex(random_bytes(50));
 require_once 'PHPMailer-6.5.1/src/PHPMailer.php';
 require_once 'PHPMailer-6.5.1/src/SMTP.php';
 require_once 'PHPMailer-6.5.1/src/Exception.php';
+
+$mail = new PHPMailer();
+try{
+    //config
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+    $mail->setLanguage('br');
+    //$mail->SMTPDebug = SMTP::DEBUG_OFF; //tira as mensagens de erro
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //imprime as mensagens de erro
+    $mail->isSMTP(); //envia o email usando SMTP
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'lorenzo.2022310934@aluno.iffar.edu.br';
+    $mail->Password = '31072005';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+} catch(Exception $e){
+    echo "Não foi possível enviar o email.
+    Mailer Erro: {$mail->ErrorInfo}";
+}
